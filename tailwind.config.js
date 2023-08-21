605a8f64c4e1a455cb050ff4d5a74abb72316bbe
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2023-08-21 19:31:54 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  extend: {
+    animation: {
+      marquee: 'marquee 25s linear infinite',
+    },
+    keyframes: {
+      marquee: {
+        '0%': { transform: 'translateX(0%)' },
+        '100%': { transform: 'translateX(-100%)' },
+      }
+    },
+  }
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
